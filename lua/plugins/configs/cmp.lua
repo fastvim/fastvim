181f4e51b5b0119fb10bdfd6720cf7a03a847197
BrunoCiccarino <landneco0@gmail.com>
2024-12-15 20:34:58 -0300

    feat: improving config for cmp

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -1,23 +1,24 @@
-local cmp = require "cmp"
+local cmp = require("cmp")
+local luasnip = require("luasnip")
 
 return {
   snippet = {
     expand = function(args)
-      require("luasnip").lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end,
   },
-  mapping = cmp.mapping.preset.insert {
+  
+  mapping = cmp.mapping.preset.insert({
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm { select = true },
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
 
-    -- luasnip
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif require("luasnip").expand_or_jumpable() then
+      elseif luasnip.expand_or_jumpable() then
         vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
       else
         fallback()
@@ -26,10 +27,11 @@ return {
       "i",
       "s",
     }),
+
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif require("luasnip").jumpable(-1) then
+      elseif luasnip.jumpable(-1) then
         vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
       else
         fallback()
@@ -38,12 +40,66 @@ return {
       "i",
       "s",
     }),
+
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+    ["<C-Space>"] = cmp.mapping.complete(),
+
+    ["<C-e>"] = cmp.mapping.abort(),
+  }),
+
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },         -- LSP completion
+    { name = "luasnip" },          -- Snippets
+    { name = "buffer" },           -- Buffer words
+    { name = "nvim_lua" },         -- nvim API completion
+    { name = "path" },             -- Path completions
+    { name = "calc" },             -- Mathematical calculations
+    { name = "emoji" },            -- Emoji completions
+  }),
+
+  window = {
+    completion = {
+      border = "rounded",   -- Rounded border for completion menu
+      winhighlight = "Normal:Pmenu,FloatBorder:PmenuBorder,Search:PmenuSel", -- Highlighting
+    },
+    documentation = {
+      border = cmp.config.window.bordered(),   -- Rounded border for documentation
+    },
   },
-  sources = cmp.config.sources {
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "buffer" },
-    { name = "nvim_lua" },
-    { name = "path" },
+
+  formatting = {
+    format = function(entry, vim_item)
+      
+      vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
+        buffer = "[Buffer]",
+        nvim_lua = "[API]",
+        path = "[Path]",
+        calc = "[Calc]",
+        emoji = "[Emoji]",
+      })[entry.source.name] or ""
+      return vim_item
+    end,
+  },
+
+  experimental = {
+    ghost_text = true,  -- Show ghost text for suggestions
+  },
+
+  completion = {
+    autocomplete = {
+      require("cmp.types").cmp.TriggerEvent.TextChanged,
+    },
+    keyword_length = 3,  -- Minimum length to trigger autocompletion
+  },
+
+  documentation = {
+    border = "single",
+    winhighlight = "NormalFloat:Normal,FloatBorder:FloatBorder,Search:Search",
   },
 }
