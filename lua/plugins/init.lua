af59c5ccedaa8673341a9d5f67a9341167af34fb
BrunoCiccarino <landneco0@gmail.com>
2024-12-18 16:15:28 -0300

    feat: adding yazi config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,7 @@
 return {
-  { lazy = true, 
+
+{ 
+  lazy = true, 
   "nvim-lua/plenary.nvim" 
 },
 {
@@ -15,6 +17,48 @@ return {
 },
 },
 {
+  "BrunoCiccarino/lazygrep.nvim"
+},
+{
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+    config = function ()
+      require("plugins.configs.yazi")
+  end
+ },
+{
+  "mrcjkb/haskell-tools.nvim",
+  version = "^3",
+  ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
+  dependencies = {
+    { "nvim-telescope/telescope.nvim", optional = true },
+  },
+  config = function()
+    local ok, telescope = pcall(require, "telescope")
+    if ok then
+      telescope.load_extension("ht")
+    end
+  end,
+},
+{
+  
+},
+{
+  "mrcjkb/haskell-snippets.nvim",
+  dependencies = { "L3MON4D3/LuaSnip" },
+  ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
+  config = function()
+    local haskell_snippets = require("haskell-snippets").all
+    require("luasnip").add_snippets("haskell", haskell_snippets, { key = "haskell" })
+  end,
+},
+{
+  "karb94/neoscroll.nvim",
+    config =function ()
+      require("plugins.configs.neoscroll")
+    end
+},
+{
   "kdheepak/lazygit.nvim",
   cmd = {
    "LazyGit",
@@ -25,9 +69,9 @@ return {
   },
   dependencies = {
    "nvim-lua/plenary.nvim",
-  },
+},
   keys = {
-   { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+  { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
   },
 },
 {
@@ -36,28 +80,28 @@ return {
     require("plugins.configs.neovim-session-manager")
   end,
 },
-  {
+{
     "EdenEast/nightfox.nvim",
     priority = 1000,
     config = true,
-  },
-  {
+},
+{
     "wtfox/jellybeans.nvim",
     priority = 1000,
     config = true,
-  },
+},
 {
   "akinsho/toggleterm.nvim",
   priority = 100,
   config = true,
 },
-    {
+{
     "nvimdev/dashboard-nvim",
     config = function()
       require("plugins.configs.ui.dashboard-nvim")
     end,
-  },
-  { 
+},
+{ 
   "catppuccin/nvim", 
   name = "catppuccin",
   -- config = function()
@@ -65,7 +109,7 @@ return {
   -- end, 
   priority = 1000 
 
-  },
+},
 {
     'jose-elias-alvarez/null-ls.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
@@ -84,12 +128,12 @@ return {
   priority = 1000
 }, 
 
-  {
+{
     'madskjeldgaard/cppman.nvim',
     dependencies = {
       'MunifTanjim/nui.nvim',
     },
-  },
+},
 
 {
   "folke/noice.nvim",
@@ -107,42 +151,42 @@ return {
     }
 },
 
-  {
+{
     "nvim-tree/nvim-tree.lua",
     cmd = { "NvimTreeToggle", "NvimTreeFocus" },
     opts = {},
-  },
+},
 
-  {
+{
     "nvim-tree/nvim-web-devicons",
     opts = {},
-  },
+},
 
-  {
+{
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
       require "plugins.configs.treesitter"
     end,
-  },
+},
 
-  {
+{
     "akinsho/bufferline.nvim",
     event = "BufReadPre",
     opts = require "plugins.configs.bufferline",
-  },
+},
 
-  {
+{
     "echasnovski/mini.statusline",
     config = function()
       require("mini.statusline").setup { set_vim_settings = false }
     end,
-  },
+},
 
   -- we use cmp plugin only when in insert mode
   -- so lets lazyload it at InsertEnter event, to know all the events check h-events
   -- completion , now all of these plugins are dependent on cmp, we load them after cmp
-  {
+{
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
@@ -157,15 +201,15 @@ return {
       "rafamadriz/friendly-snippets",
 
       -- snippets engine
-      {
+{
         "L3MON4D3/LuaSnip",
         config = function()
           require("luasnip.loaders.from_vscode").lazy_load()
         end,
-      },
+},
 
       -- autopairs , autocompletes ()[] etc
-      {
+{
         "windwp/nvim-autopairs",
         config = function()
           require("nvim-autopairs").setup()
@@ -174,16 +218,16 @@ return {
           local cmp = require "cmp"
           cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
         end,
-      },
-    },
+},
+},
     -- made opts a function cuz cmp config calls cmp module
     -- and we lazyloaded cmp so we dont want that file to be read on startup!
     opts = function()
       return require "plugins.configs.cmp"
     end,
-  },
+},
 
-  {
+{
     "williamboman/mason.nvim",
     build = ":MasonUpdate",
     cmd = { "Mason", "MasonInstall" },
@@ -191,45 +235,56 @@ return {
       ensure_installed = {
         "clangd",
         "golps"
-      }
-    },
-  },
+}
+},
+},
 
-  {
+{
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
       require "plugins.configs.lspconfig"
     end,
-  },
+},
 
-  {
+{
     "stevearc/conform.nvim",
     lazy = true,
     opts = require "plugins.configs.conform",
-  },
+},
 
-  {
+{
     "lukas-reineke/indent-blankline.nvim",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
       require("ibl").setup {
         indent = { char = "│" },
         scope = { char = "│", highlight = "Comment" },
-      }
+}
     end,
-  },
+},
 
   -- files finder etc
-  {
+{
     "nvim-telescope/telescope.nvim",
     cmd = "Telescope",
     opts = require "plugins.configs.telescope",
+},
+{
+  "nvim-neotest/neotest",
+  optional = true,
+  dependencies = {
+    { "mrcjkb/neotest-haskell" },
   },
-
-  {
+  opts = {
+    adapters = {
+      ["neotest-haskell"] = {},
+    },
+  },
+},
+{
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
     opts = {},
-  },
+},
 }
