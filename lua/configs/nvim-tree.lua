ef29af9653a3bc93a7c7333ba9f887dd39a01b78
BrunoCiccarino <landneco0@gmail.com>
2024-12-26 18:33:08 -0300

    feat: adding minimal config for nvim-tree

---
+++ lua/configs/nvim-tree.lua
@@ -0,0 +1,18 @@
+local tree = {
+  require("nvim-tree").setup({
+    sort = {
+      sorter = "case_sensitive",
+    },
+    view = {
+      width = 30,
+    },
+    renderer = {
+      group_empty = true,
+    },
+    filters = {
+      dotfiles = true,
+    },
+})
+}
+
+return tree 
