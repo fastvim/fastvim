4ddf08ef410ece87fac3032abd458c338313fd98
BrunoCiccarino <landneco0@gmail.com>
2024-12-27 21:32:54 -0300

    feat: adding dotfile loader to config

---
+++ lua/modules/loader/init.lua
@@ -0,0 +1,47 @@
+local function get_config_directory()
+  local home = vim.fn.expand('$HOME')
+  local appdata = vim.fn.getenv('APPDATA')
+  local config_dir
+
+  if vim.fn.has('unix') == 1 then
+    config_dir = home .. '/.fast.d'
+  elseif vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
+    config_dir = appdata .. '\\.fast.d'
+  else
+    config_dir = nil
+  end
+
+  return config_dir
+end
+
+local function load_config(config_dir)
+  if config_dir and vim.fn.isdirectory(config_dir) == 1 then
+    local files = vim.fn.glob(config_dir .. '/*', false, true)
+    
+    if #files > 0 then
+      for _, file in ipairs(files) do
+        local ext = vim.fn.fnamemodify(file, ':e')
+        
+        if ext == 'lua' then
+          dofile(file)
+        end
+      end
+    else
+      print("No configuration files found in: " .. config_dir)
+    end
+  else
+    print("Configuration directory does not exist: " .. config_dir)
+  end
+end
+
+local function init_config()
+  local config_dir = get_config_directory()
+  
+  if config_dir then
+    load_config(config_dir)
+  else
+    print("Unable to determine config directory.")
+  end
+end
+
+init_config()
