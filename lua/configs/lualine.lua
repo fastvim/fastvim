e8d34b053059c303ecd3a5e2998b380ada8b1ab6
BrunoCiccarino <landneco0@gmail.com>
2024-12-29 19:58:51 -0300

    ➖ Removing unused dependency

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -1,4 +1,3 @@
--- Initialize lua-line plugin
 local M = {}
 
 M.config = function()
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -127,29 +127,6 @@ return {
   }
   },
   {
-    "mrcjkb/haskell-tools.nvim",
-    version = "^3",
-    ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
-    dependencies = {
-      { "nvim-telescope/telescope.nvim", optional = true },
-    },
-    config = function()
-      local ok, telescope = pcall(require, "telescope")
-      if ok then
-        telescope.load_extension("ht")
-      end
-    end,
-  },
-  {
-    "mrcjkb/haskell-snippets.nvim",
-    dependencies = { "L3MON4D3/LuaSnip" },
-    ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
-    config = function()
-      local haskell_snippets = require('haskell-snippets').all
-      require('luasnip').add_snippets('haskell', haskell_snippets, { key = 'haskell' })
-    end,
-  },
-  {
     "karb94/neoscroll.nvim",
     config = function()
       require('configs.editor.neoscroll')
