40ee083dc986e35bfec971ee71bb746b61f49f47
BrunoCiccarino <landneco0@gmail.com>
2025-01-13 01:14:52 -0300

    🐛 Fixxing completion bug

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -1,6 +1,5 @@
 local cmp = require("cmp")
 local luasnip = require("luasnip")
-local neokinds = require("neokinds") 
 
 cmp.setup({
   snippet = {
@@ -8,80 +7,59 @@ cmp.setup({
       luasnip.lsp_expand(args.body)
     end,
   },
-
   mapping = cmp.mapping.preset.insert({
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
-["<Tab>"] = cmp.mapping(function(fallback)
-  if luasnip.expand_or_jumpable() then
-    luasnip.expand_or_jump()
-  elseif cmp.visible() then
-    cmp.select_next_item()
-  else
-    fallback()
-  end
-  end, { "i", "s" }),
-  ["<S-Tab>"] = cmp.mapping(function(fallback)
-    if luasnip.jumpable(-1) then
-      luasnip.jump(-1)
-    elseif cmp.visible() then
-      cmp.select_prev_item()
-    else
-      fallback()
-    end
-  end, { "i", "s" }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   }),
-
-sources = cmp.config.sources({
-  { name = "nvim_lsp" },
-  { name = "luasnip" },
-  { name = "buffer" },
-  { name = "nvim_lua" },
-  { name = "path" },
-  { name = "calc" },
-  { name = "emoji" },
-}),
-
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "path" },
+  }),
   window = {
     completion = cmp.config.window.bordered(),
     documentation = cmp.config.window.bordered(),
   },
-
   formatting = {
     format = function(entry, vim_item)
-      
-      vim_item.kind = string.format("%s %s", neokinds[vim_item.kind] or "", vim_item.kind)
-      vim_item.menu = ({
-        nvim_lsp = "[LSP]",
-        luasnip = "[Snippet]",
-        buffer = "[Buffer]",
-        nvim_lua = "[API]",
-        path = "[Path]",
-        calc = "[Calc]",
-        emoji = "[Emoji]",
-      })[entry.source.name] or ""
+      local kind_icons = require("neokinds") 
+      vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind] or "", vim_item.kind)
       return vim_item
     end,
   },
-
   experimental = {
     ghost_text = true,
   },
-
   performance = {
     debounce = 0,
     throttle = 0,
     fetching_timeout = 20000,
-    confirm_resolve_timeout = 1,
-    async_budget = 1,
-    max_view_entries = 100,
   },
-
   completion = {
     autocomplete = { cmp.TriggerEvent.TextChanged },
-    keyword_length = 3,
+    keyword_length = 1,
   },
 })
+
